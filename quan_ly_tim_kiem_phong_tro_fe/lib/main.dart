ec10dcd65107590bf061c894e9ef294b23e714f7
vtnguyenhttt2211025 <vtnguyenhttt2211025@student.ctuet.edu.vn>
2025-07-07 13:41:20 +0000

    test test

--- quan_ly_tim_kiem_phong_tro_fe/lib/main.dart
+++ quan_ly_tim_kiem_phong_tro_fe/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import '../features/admin/screens/home_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -33,7 +34,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+ home: HomeScreen(),
     );
   }
 }
