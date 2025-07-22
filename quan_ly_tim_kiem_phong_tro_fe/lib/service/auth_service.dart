2c967984bd7bdfbe909150ff19a309433f7763a0
Truong Nguyen <vonguyen.0407@gmail.com>
2025-07-22 21:04:21 +0700

    auth: register, login, logout

--- quan_ly_tim_kiem_phong_tro_fe/lib/service/auth_service.dart
+++ quan_ly_tim_kiem_phong_tro_fe/lib/service/auth_service.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class SignUpViewModel{
+class SignUpViewModel {
   String username = '';
   String phone = '';
   String email = '';
@@ -20,20 +21,64 @@ class AuthService {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   //====================HAM DANG KY==============
+  // Ví dụ sử dụng Firebase Auth để đăng ký và Firestore để lưu thông tin bổ sung
   Future<bool> registerUser(SignUpViewModel signUpViewModel) async {
     try {
-      await firestore.collection('users').add({
-        'username': signUpViewModel.username,
-        'phone': signUpViewModel.phone,
-        'email': signUpViewModel.email,
-        'password': signUpViewModel.password,
+      // Đăng ký với Firebase Auth
+      UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: signUpViewModel.email,
+        password: signUpViewModel.password,
+      );
+
+      // Lưu thông tin bổ sung vào Firestore
+      await firestore.collection('users').doc(userCredential.user!.uid).set({
+        'Username': signUpViewModel.username,
+        'Phone': signUpViewModel.phone,
+        'Email': signUpViewModel.email,
+        'Role': 'guest'
+        // Không lưu password dạng plain text!
       });
-      return true; // Registration successful
+
+      return true;
     } catch (e) {
       print('Error registering user: $e');
-      return false; // Registration failed
+      return false;
     }
   }
 
+
+  //=======================HAM DANG NHAP================
+  Future<Map<String, dynamic>?> loginUser(String email, String password) async {
+    try {
+      // Đăng nhập bằng Firebase Auth
+      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Lấy thông tin user từ Firestore
+      DocumentSnapshot userDoc = await firestore.collection('users').doc(userCredential.user!.uid).get();
+
+      if (userDoc.exists) {
+        Map<String, dynamic> userData = userDoc.data() as Map<String, dynamic>;
+        // Trả về thông tin user, bao gồm cả role nếu có
+        return userData;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print('Error logging in user: $e');
+      return null;
+    }
+  }
+
+  //=======================HAM DANG XUAT================
+  Future<void> logoutUser() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print('Error logging out user: $e');
+    }
+  }
 
 }
