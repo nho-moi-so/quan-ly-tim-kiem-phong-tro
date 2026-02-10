c13809770c2388a6af15ef46d7e15f2e1357c682
Truong Nguyen <vtnguyenhttt2211025@student.ctuet.edu.vn>
2026-02-10 22:03:57 +0700

    smart contract v1

--- blockchain-fabric-v2/chaincode-go/chaincode/smartcontract.go
+++ blockchain-fabric-v2/chaincode-go/chaincode/smartcontract.go
@@ -1,9 +1,5 @@
-// 1. Cấu trúc dữ liệu
-// 	User: ID, FullName, Balane, LockedBalance, Status ("ACTIVE", "PENDING", "LOCKED", "APPROVED"), Role ("ADMIN", "OWNER", "GUEST")
-// 	Apartment: ID, OwnerID, DailyRate, Status ("AVAILABLE", "BOOKED", "OCCUPIED"), PasswordHash
-// 	Contract: ID, ApartmentID, GuestID, StartDate, EndDate, EscrowAmount, Status ("CREATED", "ACTIVE", "COMPLETED")
+package chaincode
 
-<<<<<<< HEAD
 import (
 	"encoding/json"
 	"fmt"
@@ -434,31 +430,3 @@ func (s *SmartContract) GetApartmentById(ctx contractapi.TransactionContextInter
 	}
 	return &apartment, nil
 }
-=======
-// 2. Các chức năng chính
-// 	CreateUser(id, fullName, balance)
-// 	CreateApartment(id, ownerId, dailyRate) // mới tạo căn hộ, chưa có kết nối IoT nên không có mật khẩu
-// 	UpdatePasswordApartment(id, passwordHash)
-// 	BookApartment(contractId, aptId, guestId, startDate, endDate) => tiền của guest để vào biến EscrowAmount
-// 	CancelBooking(contractId)
-// 	CheckIn(contractId, newPassword)
-// 	CheckOut(contractId, resetPassword) => giải phóng tiền từ biến EscrowAmount vào cho ownerId
-// 	Deposit(userId, amount) => Cộng tiền vào Balance
-// 	RequestWithdraw(userId, amount) => Trừ Balance, cộng vào LockedBalance.
-// 	FinishWithdraw(userId, amount): Trừ LockedBalance (Xóa tiền khỏi hệ thống).
-	
-// 3. Các hàm hỗ trợ
-// 	GetAllContracts()
-// 	GetContractById(contractId)
-// 	UpdateContractById()
-	
-// 	GetAllUser()
-// 	GetUserById(userId)
-// 	UpdateUser(userId)
-	
-// 	GetAllApartment()
-// 	GetApartmentById(apartmentId)
-// 	UpdateApartment()
-	
-// 	VerifyAccess(apartmentId, passwordHash)
->>>>>>> 2a21854373817b4e22335408d5c660329ed868a8
